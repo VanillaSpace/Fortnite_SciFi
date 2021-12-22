3f419e884aee0e7a063c840aac4c531f5dd7a848
Arisa <aohashi@live.com>
2021-12-22 14:37:19 -0500

    Added Item Class
    - Each item class has its own box collider and mesh

---
+++ Source/SciFi_Shooter/Item.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+UCLASS()
+class SCIFI_SHOOTER_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	AItem();
+
+protected:
+	
+	virtual void BeginPlay() override;
+
+public:	
+	
+	virtual void Tick(float DeltaTime) override;
+
+private: 
+	// Skeletal Mesh for the item
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* ItemMesh;
+
+	// Line trace colliders with box to show HUD
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* CollisionBox;
+
+public: 
+
+
+
+};
